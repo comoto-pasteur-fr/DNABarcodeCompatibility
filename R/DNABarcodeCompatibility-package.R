352831b0572165bb00de172e925886bc2ef75ded
etournay <raphael.etournay@pasteur.fr>
2018-03-14 13:43:44 +0100

    initial commit

---
+++ R/DNABarcodeCompatibility-package.R
@@ -0,0 +1,14 @@
+#' DNABarcodeCompatibility: to find least-redundant sets of compatible barcodes for multiplex experiments performed on Illumina sequencing platforms
+#'
+#' 
+#' The DNABarcodeCompatibility package provides six groups of utility functions to analyse and select DNA barcodes for deep sequencing purposes. It also provides a pipeline to perform a complete analysis and selection of DNA barcodes: experiment_design.
+#' 
+#' @docType package
+#' @name DNABarcodeCompatibility-package
+#' @import dplyr
+#' @import tidyr
+#' @import numbers
+#' @import purrr
+#' @import stringr
+#' @import DNABarcodes
+NULL
