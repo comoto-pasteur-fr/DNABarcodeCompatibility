fef074345da34adffc6dfbb2bbf6c37f362cd340
etournay <raphael.etournay@pasteur.fr>
2018-05-31 16:22:23 +0200

    created a proper dataset with documentation

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Barcode dataset from Illumina.
+#'
+#' 48 barcodes from Illumina TruSea Small RNA kits
+#'
+#' @format A data frame with 48 rows and 2 variables:
+#' \describe{
+#'   \item{V1}{barcode identifier}
+#'   \item{V2}{DNA sequence}
+#'   ...
+#' }
+#'
+"IlluminaIndexes"
