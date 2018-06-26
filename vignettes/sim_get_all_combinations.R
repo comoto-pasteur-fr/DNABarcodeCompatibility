5c3ed57efd6736323849fd9e4e66830ffb113add
etournay <raphael.etournay@pasteur.fr>
2018-06-26 23:21:48 +0200

    set first simulation on new barcode set

--- vignettes/sim_get_all_combinations.R
+++ vignettes/sim_get_all_combinations.R
@@ -3,7 +3,7 @@
 ## Setup  I/O ####
 argv = commandArgs(TRUE)
 
-if((length(argv) < 2) | (length(argv) > 6)){
+if((length(argv) < 2) | (length(argv) > 7)){
   stop("Usage: time_simulation_s.R <mplex_level> <barcode_number> <chemistry> <outputfile1> <outputfile2>")
 }else{
   rep_number=as.integer(argv[1])
@@ -12,11 +12,15 @@ if((length(argv) < 2) | (length(argv) > 6)){
   chemistry=as.integer(argv[4])
   outputfile1=normalizePath(argv[5])
   outputfile2=normalizePath(argv[6])
+  inputfile=normalizePath(argv[7])
 }
 
+
 ## Main ####
+dna_barcode_set <- local(get(load(inputfile)))
+
 nb_total_combinations <- as.numeric(choose(barcode_number, mplex_level))
-index <- dplyr::sample_n(DNABarcodeCompatibility::IlluminaIndexes, nrow(DNABarcodeCompatibility::IlluminaIndexes), replace = FALSE)
+index <- dplyr::sample_n(dna_barcode_set, nrow(dna_barcode_set), replace = FALSE)
 elapsed <- as.numeric(system.time(compatible_comb <- DNABarcodeCompatibility::get_all_combinations(index_df = index[1:barcode_number,], mplex_level = mplex_level, chemistry = chemistry))[3])
 nb_comp_combinations <- nrow(compatible_comb)
 
