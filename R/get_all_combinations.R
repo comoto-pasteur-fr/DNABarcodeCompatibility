d0e6f727e41c8924b87283ccf5b280b38ad88009
etournay <raphael.etournay@pasteur.fr>
2018-11-25 13:25:25 +0100

    Fixed the controls on the multiplex level for both `get_random_combinations()` and `get_all_combinations()`

--- R/get_all_combinations.R
+++ R/get_all_combinations.R
@@ -10,6 +10,7 @@
 #' @param index_df A dataframe containing barcodes identifiers, corresponding
 #'  DNA sequences along with GC content and presence of homopolymers.
 #' @param mplex_level The number at which the barcodes will be multiplexed.
+#' Illumina recommends to not multiplex more than 96 libraries.
 #' @param platform An integer representing the number of channels (1, 2, 4)
 #' of the desired Illumina platform: 1 for iSeq; 2 for NextSeq, NovaSeq,
 #' MiniSeq; 4 for HiSeq and MiSeq. 0 represents any other platform than
@@ -35,32 +36,38 @@
 get_all_combinations = function(index_df, mplex_level, platform){
     if (is.numeric(mplex_level)){
         if(mplex_level <= 96){
-    combination_m = NULL
-        if (platform == 4 ){
-            combinations_m = get_all_combinations_4_channel(index_df,
-                                                            mplex_level)
-        } else if (platform == 2){
-            combinations_m = get_all_combinations_2_channel(index_df, 
-                                                            mplex_level)
-        } else if (platform == 1){
-            combinations_m = get_all_combinations_1_channel(index_df, 
-                                                            mplex_level)
-        } else if (platform == 0){
-            combinations_m = get_all_combinations_0_channel(index_df, 
-                                                            mplex_level)
-        } else {
-            display_message("Please choose a correct platform 
-                                    for your experiment ")
-        }
-        if(nrow(as.matrix(combinations_m)) == 0){
-            display_message(paste("The programm didn't find any compatible", 
-                                "combination, please check your index list",
-                                "and your search parameters"))
-            return (NULL)
-        }else{
-            return (combinations_m)
-        }
-        }else{display_message(
+            if (mplex_level <= nrow(index_df)){
+                combination_m = NULL
+                if (platform == 4 ){
+                    combinations_m = get_all_combinations_4_channel(index_df,
+                                                                    mplex_level)
+                } else if (platform == 2){
+                    combinations_m = get_all_combinations_2_channel(index_df, 
+                                                                    mplex_level)
+                } else if (platform == 1){
+                    combinations_m = get_all_combinations_1_channel(index_df, 
+                                                                    mplex_level)
+                } else if (platform == 0){
+                    combinations_m = get_all_combinations_0_channel(index_df, 
+                                                                    mplex_level)
+                } else {
+                    display_message("Please choose a correct platform 
+                                        for your experiment ")
+                }
+                if(nrow(as.matrix(combinations_m)) == 0){
+                    display_message(paste("The programm didn't find any",
+                                          "compatible combination, please check", 
+                                          "your index list and your search",
+                                          "parameters"))
+                    return (NULL)
+                }else{
+                    return (combinations_m)
+                }
+            } else {display_message(paste("The value of mplex_level",
+                                          " should not be higher than the",
+                                          "number of available barcodes"))
+            }
+        } else {display_message(
             "The value of mplex_level should not be higher than 96")
         }
     }else{
--- R/get_random_combinations.R
+++ R/get_random_combinations.R
@@ -11,6 +11,7 @@
 #' @param index_df A dataframe containing barcodes identifiers, corresponding
 #' DNA sequences along with GC content and presence of homopolymers.
 #' @param mplex_level The number at which the barcodes will be multiplexed.
+#' Illumina recommends to not multiplex more than 96 libraries.
 #' @param platform An integer representing the number of channels (1, 2, 4)
 #' of the desired Illumina platform: 1 for iSeq; 2 for NextSeq, NovaSeq,
 #' MiniSeq; 4 for HiSeq and MiSeq. 0 represents any other platform than
@@ -36,35 +37,41 @@
 get_random_combinations = function(index_df, mplex_level, platform){
     if (is.numeric(mplex_level)){
         if(mplex_level <= 96){
-            combinations_m = NULL
-            if (platform == 4 ){
-                combinations_m = get_random_combinations_4_channel(index_df,
-                                                                   mplex_level)
-            } else if (platform == 2){
-                combinations_m = get_random_combinations_2_channel(index_df,
-                                                                   mplex_level)
-            } else if (platform == 1){
-                combinations_m = get_random_combinations_1_channel(index_df,
-                                                                   mplex_level)
-            } else if (platform == 0){
-                combinations_m = get_random_combinations_0_channel(index_df,
-                                                                   mplex_level)
-            } else {
-                display_message(paste("Please choose a correct platform",
-                                      "for your experiment"))
-                return(NULL)
+            if (mplex_level <= nrow(index_df)){
+                combinations_m = NULL
+                if (platform == 4 ){
+                    combinations_m = get_random_combinations_4_channel(index_df,
+                                                                       mplex_level)
+                } else if (platform == 2){
+                    combinations_m = get_random_combinations_2_channel(index_df,
+                                                                       mplex_level)
+                } else if (platform == 1){
+                    combinations_m = get_random_combinations_1_channel(index_df,
+                                                                       mplex_level)
+                } else if (platform == 0){
+                    combinations_m = get_random_combinations_0_channel(index_df,
+                                                                       mplex_level)
+                } else {
+                    display_message(paste("Please choose a correct platform",
+                                          "for your experiment"))
+                    return(NULL)
+                }
+                if(nrow(as.matrix(combinations_m)) == 0){
+                    display_message(paste(
+                        "The programm didn't find any compatible", 
+                        
+                        "combination, please check your index list",
+                        
+                        "and your search parameters"))
+                    return (NULL)
+                }else{
+                    return (combinations_m)
+                }
+            } else {display_message(paste("The value of mplex_level",
+                                          " should not be higher than the",
+                                          "number of available barcodes"))
             }
-            if(nrow(as.matrix(combinations_m)) == 0){
-                display_message(paste(
-                    "The programm didn't find any compatible", 
-                                      
-                    "combination, please check your index list",
-                                      
-                    "and your search parameters"))
-                return (NULL)
-            }else{
-                return (combinations_m)
-            }
+                
         }else{display_message(
             "The value of mplex_level should not be higher than 96")
         }
